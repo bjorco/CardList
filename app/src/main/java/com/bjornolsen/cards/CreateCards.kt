6d78e4877f0189dc9b4882fea4ad89ad044ccbfb
Bjørn Olsen <bctorpolsen@gmail.com>
2019-02-15 19:35:20 +0100

    Functionality to CreateCard acitiy

--- app/src/main/java/com/bjornolsen/cards/CreateCards.kt
+++ app/src/main/java/com/bjornolsen/cards/CreateCards.kt
@@ -1,12 +1,15 @@
 package com.bjornolsen.cards
 
+import android.arch.persistence.room.Room
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_create_cards.*
+import kotlinx.android.synthetic.main.content_create_cards.*
 
 class CreateCards : AppCompatActivity() {
+
+    private val TAG = this.javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +17,20 @@ class CreateCards : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            insertCard(cc_edt_card_name.text.toString(),cc_edt_card_number.text.toString().toInt())
         }
+    }
+
+    //TODO: Move to viewmodel
+    private fun insertCard(name : String, number : Int){
+        Log.d(TAG, "insertCard: name:$name & number:$number")
+        //TODO: Add dependency injection and use a background thread
+        val db = Room.databaseBuilder(applicationContext, Database::class.java, "cardsDatabase")
+            .allowMainThreadQueries()
+            .build()
+
+        db.cardDao().insert(Card(0,name,number))
+
+        finish()
     }
 }
--- app/src/main/java/com/bjornolsen/cards/MainActivity.kt
+++ app/src/main/java/com/bjornolsen/cards/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -23,24 +24,23 @@ class MainActivity : AppCompatActivity() {
         populateRecyclerView()
     }
 
-    fun populateRecyclerView(){
-
+    private fun populateRecyclerView(){
         //TODO: Add dependency injection and use a background thread
         val db = Room.databaseBuilder(applicationContext, Database::class.java, "cardsDatabase")
             .allowMainThreadQueries()
             .build()
 
-        /*
-        for (i in 1..10){
-            val c = Card(0,"Dunder$i",i)
-            db.cardDao().insert(c)
-        }
-        */
-
         val cards = ArrayList<Card>(db.cardDao().getAllCards())
+
+        Log.d(TAG, "populateRecyclerView with ${cards.size} items")
 
         recycler_view.adapter = CardAdapter(this, cards)
         recycler_view.layoutManager = LinearLayoutManager(this)
-        recycler_view.addItemDecoration(DividerItemDecoration(recycler_view.context, DividerItemDecoration.VERTICAL))
+        recycler_view.addItemDecoration(DividerItemDecoration(applicationContext, DividerItemDecoration.VERTICAL))
+    }
+
+    override fun onResume() {
+        populateRecyclerView()
+        super.onResume()
     }
 }
